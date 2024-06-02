bb7cc9fadc0e6ce40afba659be2cf79d39a4db2d
jean_ <jean_@Jean-NT>
2024-06-02 09:00:06 -0300

    mudança para Mysql

--- src/main/java/com/example/demo/LojaVirtualApplication.java
+++ src/main/java/com/example/demo/LojaVirtualApplication.java
@@ -8,6 +8,7 @@ public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
---
+++ src/main/java/com/example/demo/model/ProdutoMarca.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "produto_marca")
+public class ProdutoMarca implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name= "id")
+	private long id;
+	@Column(name="marca")
+	private String marca;
+
+}
